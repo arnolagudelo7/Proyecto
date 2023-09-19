d1e57dd072e855b90650bc79aae487c75b7a6bd8
arnolagudelo7 <arnol5417@gmail.com>
2023-09-19 10:05:13 -0500

    cambio

--- Proyecto/Views.py
+++ Proyecto/Views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render,redirect
 #from Pet.models import
+
+#region index
+
+def Index(request):    
+    return render(request, 'Index.html')
+
+#endregion
+
+#region Login
+
+def Registro(request):    
+    return render(request, 'Login/Registro.html')
+
+def Ingreso(request):    
+    return render(request, 'Login/Ingreso.html')
+
+#endregion
--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .Views import Index,Registro,Ingreso
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Index
+    path('Index/',Index),
+
+    #Registro
+    path('Registro/',Registro),
+
+    #Ingreso
+    path('Ingreso/',Ingreso)
 ]
